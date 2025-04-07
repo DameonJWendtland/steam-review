144d05ef2d2562d2e5c80dfc019702b4a4d17f92
Dameon Jérôme Wendtland <github@dameon-wendtland.de>
2025-04-07 23:32:55 +0200

    relocate icons

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
-# main.py
+# src/main.py
 import tkinter as tk
 from gui.app import SteamReviewGeneratorApp
-
 
 def main():
     root = tk.Tk()
     root.title("Steam Review Generator")
     root.geometry("800x600")
     root.resizable(True, True)
-    root.iconbitmap("../icons/checkliste.ico")
-
+    root.iconbitmap("checkliste.ico")
     app = SteamReviewGeneratorApp(root)
     root.mainloop()
-
 
 if __name__ == '__main__':
     main()
